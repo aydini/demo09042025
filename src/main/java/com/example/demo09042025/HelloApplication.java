1b90203771b5876a12e6259ad224a86e306a5ad7
ilknur Aydin <aydini@farmingdale.edu>
2025-09-04 12:36:41 -0400

    Change window title again

--- src/main/java/com/example/demo09042025/HelloApplication.java
+++ src/main/java/com/example/demo09042025/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello CSC 325 Class!!!");
+        stage.setTitle("**** Hello CSC 325 Class ****");
         stage.setScene(scene);
         stage.show();
     }
